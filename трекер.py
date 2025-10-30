1a0e2cc740a3de6cd43cde433a3bfef7a3642d68
Glab <glebpolivoda@gmail.com>
2025-10-30 16:58:20 +0300

    переделал

--- трекер.py
+++ трекер.py
@@ -1,7 +1,16 @@
 from tkinter import *
 
 window = Tk()
-window.title("Мой трекер учебы")
-window.geometry("1920x1080")
+window.title("Мой трекер учебы - Глеб")
+window.state("zoomed")  # Максимизировать окно
+
+# Можно добавить меню для управления
+menubar = Menu(window)
+file_menu = Menu(menubar, tearoff=0)
+file_menu.add_command(label="Выход", command=window.destroy)
+menubar.add_cascade(label="Файл", menu=file_menu)
+
+window.config(menu=menubar)
+
 
 window.mainloop()
